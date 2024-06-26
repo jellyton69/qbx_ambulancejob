fcc54e2e92b7bfed9167c69ade1236312702d63f
Palycraft <palycraft@gmail.com>
2024-06-25 20:14:00 -0400

    final working version.

--- client/laststand.lua
+++ client/laststand.lua
@@ -28,33 +28,3 @@ end)
 lib.callback.register('hospital:client:canHelp', function()
     return exports.qbx_medical:getLaststand() and exports.qbx_medical:getLaststandTime() <= 300
 end)
-
----@param targetId number playerId
-RegisterNetEvent('hospital:client:HelpPerson', function(targetId)
-    if GetInvokingResource() then return end
-    if lib.progressCircle({
-            duration = math.random(30000, 60000),
-            position = 'bottom',
-            label = locale('progress.revive'),
-            useWhileDead = false,
-            canCancel = true,
-            disable = {
-                move = false,
-                car = false,
-                combat = true,
-                mouse = false,
-            },
-            anim = {
-                dict = HealAnimDict,
-                clip = HealAnim,
-            },
-        })
-    then
-        ClearPedTasks(cache.ped)
-        exports.qbx_core:Notify(locale('success.revived'), 'success')
-        TriggerServerEvent('hospital:server:RevivePlayer', targetId)
-    else
-        ClearPedTasks(cache.ped)
-        exports.qbx_core:Notify(locale('error.canceled'), 'error')
-    end
-end)
